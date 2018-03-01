dcc7a57927935bfab0b99101cfb4fec873a5f875
He YongLiang <heyl2804@gmail.com>
2018-03-01 10:01:57 +0800

    IM 替换

---
+++ library/TIM_SDK/src/main/java/com/tencent/qcloud/sdk/Constant.java
@@ -0,0 +1,12 @@
+package com.tencent.qcloud.sdk;
+
+/**
+ * 常量
+ */
+public class Constant {
+
+    public static final int ACCOUNT_TYPE = 21839 ;
+    //sdk appid 由腾讯分配
+    public static final int SDK_APPID = 1400063737;
+
+}
